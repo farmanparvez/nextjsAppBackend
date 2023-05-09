4b50e5cd7fbac84e1298795cf30bc55c3c8ac2b3
farman <farman@kmindz.in>
2023-05-09 12:14:42 +0530

    fixed

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
 
-const corsOption = {
-    origin: ['http://localhost:3000'],
-    optionsSuccessStatus: 200
+const corsOptions = {
+    credentials: true,
+    origin: "http://localhost:3000", // Add your frontend origin here (Don't add '/' at the end)
+    optionsSuccessStatus: 200,
 };
 
 app.use(cors())
-app.options('*', cors())
+app.use("*", cors(corsOptions)); // npm i cors
 app.use(express.json())
 app.get('/', (req, res) => res.status(200).json({
     working: 'working'
